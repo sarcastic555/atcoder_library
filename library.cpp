c6871e1224bc3a5b557b7e88404dddb9d22d5fe7
sarcastic555 <naoki@naoki-2.local>
2020-01-13 18:56:15 +0900

    add inverse mod calculation

--- library.cpp
+++ library.cpp
@@ -7,6 +7,8 @@
 #include <thread>
 #include <vector>
 
+long long mod = 1000000007;
+
 // aとbの最大公約数を求める
 long long evalmod(long long a, long long b){
   long long maxval = std::max(a, b);
@@ -16,4 +18,20 @@ long long evalmod(long long a, long long b){
   }
   long long tmpval = maxval%minval;
   return evalmod(minval, tmpval);
+}
+
+// a^n mod を計算する
+long long modpow(long long a, long long n, long long mod) {
+  long long res = 1;
+  while (n > 0) {
+    if (n & 1) res = res * a % mod;
+    a = a * a % mod;
+    n >>= 1;
+  }
+  return res;
+}
+
+// a^{-1} mod を計算する
+long long modinv(long long a, long long mod) {
+  return modpow(a, mod - 2, mod);
 }
