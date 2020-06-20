0e57a07522ace78476599d64e50a979d546ac82d
sarcastic555 <Unkown>
2020-06-20 11:01:02 +0900

    add infinite loop to compare.sh

--- compare.sh
+++ compare.sh
@@ -1,15 +1,26 @@
 #!/bin/bash
 
-INPUTFILE="input.txt"
+INPUTFILE="${PWD}/input.txt"
 
 g++ test.cpp -o test.out
-g++ test2.cpp -o test2.out
+g++ test3.cpp -o test3.out
 
-./random_generator/field_generator.sh ${INPUTFILE}
+while true; do
+    echo ${INPUTFILE}
+    #source ./random_generator/field_generator.sh ${INPUTFILE}
+    source ./random_generator/string_generator.sh ${INPUTFILE}
 
-cat ${INPUTFILE}
-./test.out < ${INPUTFILE} > out.txt
-./test2.out < ${INPUTFILE} > out2.txt
-
-diff -u out.txt out2.txt
-	 
+    echo "====== show input start ===="
+    cat ${INPUTFILE}
+    echo "====== show input end ===="
+    ./test.out < ${INPUTFILE} > out.txt
+    ./test3.out < ${INPUTFILE} > out3.txt
+    echo "=== diff output start ==="
+    diff -u out.txt out3.txt
+    echo "=== diff output end ==="
+    diffcount=`diff -u out.txt out3.txt | wc -l`
+    if [ $diffcount -ne 0 ]; then
+	break
+    fi
+    sleep 0.1
+done
