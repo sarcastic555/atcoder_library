f1c26fe2fffbcb8ada50a8d3dca151c56b01a8de
sarcastic555 <Unkown>
2020-06-28 13:15:05 +0900

    add mint class to mod calculation

--- library.cpp
+++ library.cpp
@@ -21,19 +21,6 @@ bool sortcol3_inv(std::vector<long long>& v1, std::vector<long long>& v2){
   return sortcol(v2, v1, 3);
 }
 
-long long mod = 1000000007;
-
-// aとbの最大公約数を求める
-long long evalmod(long long a, long long b){
-  long long maxval = std::max(a, b);
-  long long minval = std::min(a, b);
-  if (minval==0){
-    return maxval;
-  }
-  long long tmpval = maxval%minval;
-  return evalmod(minval, tmpval);
-}
-
 bool isprime(int n){
   bool prime = true;
   for (int i=2; i*i<=n; i++){
@@ -43,34 +30,4 @@ bool isprime(int n){
     }
   }
   return prime;
-}
-
-// a^n mod を計算する
-long long modpow(long long a, long long n, long long mod) {
-  long long res = 1;
-  while (n > 0) {
-    if (n & 1) res = res * a % mod;
-    a = a * a % mod;
-    n >>= 1;
-  }
-  return res;
-}
-
-// a^{-1} mod を計算する
-long long modinv(long long a, long long mod) {
-  return modpow(a, mod - 2, mod);
-}
-
-// nCk @mod を計算する
-long long nCk(long long n, long k, long long mod){
-  long long retval = 1;
-  for (int i=0; i<k; i++){
-    retval *= (n-i);
-    retval %= mod;
-  }
-  for (int i=1; i<=k; i++){
-    retval *= modinv(i, mod);
-    retval %= mod;
-  }
-  return retval;
 }
---
+++ mint.cpp
@@ -0,0 +1,95 @@
+long long mod = 1e9+7;
+class Mint {
+public:
+  long long x;
+  Mint(){}; // 引数なしでも定義できるように引数なしコンストラクタも用意しておく
+  Mint(long long a){
+    x = a%mod;
+    while(x<0){
+      x += mod;
+    }
+  };
+  Mint& operator += (const Mint &a){
+    x+=a.x;
+    x%=mod;
+    return *this;
+  }
+  Mint& operator -= (const Mint &a){
+    x += (mod - a.x);
+    x%=mod;
+    return *this;
+  }
+  Mint& operator *= (const Mint &a){
+    x*=a.x;
+    x%=mod;
+    return *this;
+  }
+
+
+  // a^n mod を計算する
+  Mint pow(Mint a, long long n) const {
+    Mint y(1);
+    while (n > 0) {
+      if (n & 1) y *= a;
+      a *= a;
+      n >>= 1;
+    }
+    return y;
+  }
+
+  // a^{-1} mod を計算する
+  Mint modinv(const Mint &a) const {
+    return pow(a, mod-2);
+  }
+
+  Mint& operator /= (const Mint &a) {
+    x *= modinv(a).x;
+    x %= mod;
+    return *this;
+  }
+
+  Mint operator + (Mint &a) const {
+    Mint y(*this);
+    y += a;
+    return y;
+  }
+
+  Mint operator - (Mint &a) const {
+    Mint y(*this);
+    y -= a;
+    return y;
+  }
+
+  Mint operator * (Mint &a) const {
+    Mint y(*this);
+    y *= a;
+    return y;
+  }
+
+  Mint operator / (Mint &a) const {
+    Mint y(*this);
+    y/=a;
+    return y.x;
+  }
+
+  // nCk @mod を計算する
+  Mint nCk(Mint &n, const long long k) const {
+    Mint y = Mint(1);
+    for (Mint i(0); (i.x)<k; i.x++){
+      y *= (n-i);
+      y /= i;
+    }
+    return y;
+  }
+
+  // nPk @mod を計算する
+  Mint nPk(Mint &n, long long k) const {
+    Mint y(1);
+    for (Mint i(0); (i.x)<k; i.x++){
+      y *= (n-i);
+    }
+    return y;
+  }
+};
+
+
