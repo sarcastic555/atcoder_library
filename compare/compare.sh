218046bcfd22ed5ddf8335efcb129b961d9835d7
Naoki Nagakura <nagakura@nagakuranaokis-MacBook-Pro.local>
2020-12-31 10:46:55 +0900

    add tree generator script

--- compare/compare.sh
+++ compare/compare.sh
@@ -4,31 +4,35 @@ set -eu
 INPUTFILE="${PWD}/input.txt"
 TARGET1="test.cpp"
 TARGET2="test2.cpp"
-OUT1="test.out"
-OUT2="test2.out"
-RESULT1="out.txt"
-RESULT2="out2.txt"
+RESULTDIR="result"
+OUT1="${RESULTDIR}/test.out"
+OUT2="${RESULTDIR}/test2.out"
+RESULT1="${RESULTDIR}/out.txt"
+RESULT2="${RESULTDIR}/out2.txt"
 
-g++ ${TARGET1} -o ${OUT1}
-g++ ${TARGET2} -o ${OUT2}
+mkdir -p ${RESULTDIR}
+g++ -std=c++17 ${TARGET1} -o ${OUT1}
+g++ -std=c++17 ${TARGET2} -o ${OUT2}
 
+count=1
 while true; do
-    echo ${INPUTFILE}
-    #source ./random_generator/field_generator.sh ${INPUTFILE}
-    #source ./random_generator/string_generator.sh ${INPUTFILE}
-    source ./random_generator/integer_array_generator.sh ${INPUTFILE}
-
-    echo "====== show input start ===="
+    # Generate random input
+    ./random_generator/generate_random_tree.sh ${INPUTFILE}
+    echo "====== Trial ${count} ====="
+    echo "Input===:"
     cat ${INPUTFILE}
-    echo "====== show input end ===="
+
+    # Execution
     ./${OUT1} < ${INPUTFILE} > ${RESULT1}
     ./${OUT2} < ${INPUTFILE} > ${RESULT2}
-    echo "=== diff output start ==="
-    diff -u ${RESULT2} ${RESULT2}
-    echo "=== diff output end ==="
+
+    # Check diff
     diffcount=`diff -u ${RESULT1} ${RESULT2} | wc -l`
     if [ $diffcount -ne 0 ]; then
+	echo "Diff found===:"
+	diff -u ${RESULT1} ${RESULT2}
 	break
     fi
     sleep 0.1
+    count=`expr $count + 1`
 done
---
+++ compare/random_generator/generate_random_tree.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# This first creates input corresponding to tree structure, then selects a pair of nodes
+#
+# ===== Format =====
+# N
+# x_1 y_1
+# x_2 y_2
+# ...
+# x_N-1 y_N-1
+# 1
+# x y
+# ==================
+# Ref: https://atcoder.jp/contests/abc014/tasks/abc014_4
+
+## Parameters
+OUTFILE=$1
+SCRIPTDIR=$(cd $(dirname $0); pwd)
+N=20
+
+## Compile
+g++ -std=c++17 ${SCRIPTDIR}/tree_generator.cpp -o ${SCRIPTDIR}/tree_generator
+
+## Main
+echo $N > $OUTFILE
+${SCRIPTDIR}/tree_generator $N $OUTFILE
+echo 1 >> $OUTFILE
+rand1=`expr ${RANDOM} % ${N} + 1`
+rand2=`expr ${RANDOM} % ${N} + 1`
+while [ $rand1 -eq $rand2 ]; do
+    rand1=`expr ${RANDOM} % ${N} + 1`
+    rand2=`expr ${RANDOM} % ${N} + 1`
+done
+echo $rand1 $rand2 >> $OUTFILE
+
